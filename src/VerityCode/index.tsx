1192b12b0c2ec331b1fcd60cbe566cbb64933cf1
hahaha <1802758314@qq.com>
2024-01-18 16:31:29 +0800

    chore: 遗留问题、自动填充验证码错误、微信浏览器输入法闪动

--- src/VerityCode/index.tsx
+++ src/VerityCode/index.tsx
@@ -1,5 +1,13 @@
-import React, { ChangeEventHandler, useMemo, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './index.scss';
+const KEYCODE = Object.freeze({
+  LEFT_ARROW: 'ArrowLeft',
+  RIGHT_ARROW: 'ArrowRight',
+  END: 'End',
+  HOME: 'Home',
+  SPACE: 'Space',
+  BACK_SPACE: 'Backspace',
+});
 
 interface IVerityCode extends React.HTMLAttributes<HTMLDivElement> {
   bit?: number;
@@ -9,64 +17,117 @@ interface IVerityCode extends React.HTMLAttributes<HTMLDivElement> {
 const VerityCode = ({
   bit = 6,
   onFinish,
-  inputProps = {},
+  inputProps,
   ...props
 }: IVerityCode) => {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = React.useState<string[]>(new Array(bit).fill(''));
+  // 用来存放6个input的引用
+  const inputsRef = React.useRef<HTMLInputElement[]>([]);
   const [focusIndex, setFocusIndex] = useState(0);
-  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const newValue = e.target.value;
-    setFocusIndex(e.target.selectionEnd || 0);
-    setValue(newValue);
-
-    if (newValue.length === bit) {
-      onFinish?.(newValue);
-    }
-  };
-
-  const handleFocus = () => {
-    if (!inputRef.current) {
+  // 聚焦指定下标的input
+  const focusInput = (i: number) => {
+    const input = inputsRef.current?.[i];
+    if (!input) {
       return;
     }
-    setFocusIndex(inputRef.current.selectionEnd || 0);
-    inputRef.current.focus();
+    input.focus();
+    input.setSelectionRange?.(0, 1000);
+    setFocusIndex(i);
   };
 
-  const handleBlur = () => {
-    setFocusIndex(-1);
+  // 聚焦后一个input
+  const focusNextInput = () => {
+    focusInput(focusIndex + 1);
   };
 
-  const renderArray = useMemo(() => {
-    return value.padEnd(bit, ' ').split('');
-  }, [focusIndex, value]);
+  // 聚焦前一个input
+  const focusPrevInput = () => {
+    focusInput(focusIndex - 1 <= 0 ? 0 : focusIndex - 1);
+  };
+
+  const changeValue = (v: string[]) => {
+    setValue(v);
+    if (!v.some((i) => !i)) {
+      onFinish?.(v.join(''));
+    }
+  };
+
+  const setNewValue = (v: string, index: number) => {
+    const newValue = [...value];
+    newValue[index] = v;
+    changeValue(newValue);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const val = e.target.value.trim();
+    const newValue = val.slice(val.length - 1, val.length);
+    setNewValue(newValue, index);
+    if (val) {
+      focusNextInput();
+    }
+  };
+
+  // 删除
+  const handleOnDelete = (index: number) => {
+    if (value[index]) {
+      setNewValue('', index);
+    } else {
+      focusPrevInput();
+    }
+  };
+
+  const handleOnKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    switch (e.code) {
+      case KEYCODE.HOME:
+      case KEYCODE.END:
+      case KEYCODE.SPACE:
+        e.preventDefault();
+        break;
+      case KEYCODE.BACK_SPACE:
+        handleOnDelete(index);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const copyValue = e.clipboardData.getData('text/plain').slice(0, bit);
+    const len = copyValue.length;
+    const index = len === bit ? bit - 1 : len;
+    changeValue(copyValue.padEnd(bit, '').split(''));
+    focusInput(index);
+  };
 
   return (
-    <div className="feReactPopVerityCode" {...props}>
-      <div className="displayArea">
-        {renderArray.map((item, index) => (
-          <div
-            className={`value ${focusIndex === index ? 'focus' : ''}`}
+    <div className={'feReactPopVerityCode'} {...props}>
+      {value.map((v, index) => {
+        const focus = index === focusIndex;
+        return (
+          <input
+            autoFocus={index === 0}
             key={index}
-            onClick={handleFocus}
-          >
-            {item}
-          </div>
-        ))}
-      </div>
-
-      <input
-        className="hideInput"
-        autoFocus
-        ref={inputRef}
-        value={value}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        maxLength={bit}
-        autoComplete="one-time-code"
-        {...inputProps}
-      />
+            ref={(ref: HTMLInputElement) => (inputsRef.current[index] = ref)}
+            className={`feReactPopVerityCodeInput ${focus ? 'focus' : ''}`}
+            maxLength={1}
+            value={v || ''}
+            // autoComplete="one-time-code"
+            onClick={() => focusInput(index)}
+            onChange={(e) => handleChange(e, index)}
+            onKeyDown={(e) => handleOnKeyDown(e, index)}
+            onPaste={handlePaste}
+            {...inputProps}
+          />
+        );
+      })}
     </div>
   );
 };
